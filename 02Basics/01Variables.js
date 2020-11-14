eab6c6fea3612e0b8965b03ef5b1f829e5f9ea5a
hbrashid <hbrashid@gmail.com>
2020-11-14 01:05:30 -0600

    Variables

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,14 @@
+var fullName = "Hassan Rashid";
+
+
+var courseName = "React JS Bootcamp";
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+// console.log(loggedCount)
+
+var paymentMode;
+
+paymentMode = "Credit Card";
+console.log(paymentMode)

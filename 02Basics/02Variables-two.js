1d96c6f7198cc4b4e112dcfe7c710ef5a2f5dd55
hbrashid <hbrashid@gmail.com>
2020-11-14 01:23:35 -0600

    more variables

---
+++ 02Basics/02Variables-two.js
@@ -0,0 +1,25 @@
+const uid = "abc123";
+
+var firstName = "Hassan";
+var lastName = "Rashid"
+var email = "hbrashid@gmail.com";
+var country = "USA";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+var isLoggedInFromFacebook = false;
+
+// fullName = prompt("Enter your name");
+
+
+console.log(email);
+console.log(uid);
+
+console.log(`
+    With Unique ID: ${uid}
+    User is: ${firstName} ${lastName}
+    and his email is: ${email}
+    and his country is: ${country}
+    and uses the password: ${password} 
+`)

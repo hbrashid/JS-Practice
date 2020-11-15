5e2015361302213786a91f8e2f53ca94555a0280
hbrashid <hbrashid@gmail.com>
2020-11-15 00:10:35 -0600

    more practice, conditionals

---
+++ 02Basics/04Conditional.js
@@ -0,0 +1,16 @@
+var temperature;
+
+// TODO: Go to Google and get some data
+
+temperature = 39;
+
+if (temperature < 20) {
+    console.log("It's very cold outside")
+}
+
+if (temperature < 30) {
+    console.log("It's moderate outside");
+} else {
+    console.log("It's really HOT outside");
+}
+

870674d47bf424d618437f852c576c4fc002e24d
hbrashid <hbrashid@gmail.com>
2020-11-17 22:58:49 -0600

    functions

---
+++ 03Intermediate/01functions.js
@@ -0,0 +1,15 @@
+function sayHello(name) {
+    console.log("Hello there, Hassan");
+    console.log(`Hello there, ${name}`);
+}
+
+sayHello("Dude")
+
+function hello() {
+    return "Hello in America";
+}
+
+let greeting = hello();
+
+console.log(greeting);
+console.log(hello())

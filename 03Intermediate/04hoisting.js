f803c39b15b2da8bb17191ee833ea050379a85c9
hbrashid <hbrashid@gmail.com>
2020-11-21 23:43:31 -0600

    hoisting

---
+++ 03Intermediate/04hoisting.js
@@ -0,0 +1,25 @@
+
+    tipper("5")
+
+
+function tipper(a) {
+    var bill = parseInt(a)
+    console.log(bill+5)
+}
+
+bigTipper(200)
+function bigTipper(a) {
+    var bill = parseInt(a)
+    console.log(bill+15)
+}
+
+console.log(name)
+var name = "Hassan";
+
+function sayName() {
+    var name = "Mr. Hassan";
+    console.log(name)
+}
+sayName();
+
+console.log(name);

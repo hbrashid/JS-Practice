63e74b8510e6bb84f996000af1bce927bdf33a19
hbrashid <hbrashid@gmail.com>
2020-11-22 22:53:08 -0600

    Scope

---
+++ 03Intermediate/05scopeChaining.js
@@ -0,0 +1,14 @@
+var name = "Hassan";
+
+console.log("Line number 3", name);
+
+function sayName() {
+    // var name = "Mr. H"
+    console.log("Line number 7", name);
+    sayNameTwo();
+
+    function sayNameTwo() {
+        console.log("Line number 11", name)
+    }
+}
+sayName()

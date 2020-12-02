c7d369518f44fc2200cee40f9d57285bf63db041
hbrashid <hbrashid@gmail.com>
2020-12-01 23:49:17 -0600

    arrays2

--- 03Intermediate/07array.js
+++ 03Intermediate/07array.js
@@ -2,16 +2,24 @@ var countries = ["Pakistan", "USA", "Fiji", "UK"];
 
 var states = new Array("Texas", "Colorado", "California", "Iowa");
 
-console.log(states[0]);
+// console.log(states[0]);
 
-console.log(states.length)
+// console.log(states.length)
 
 states[3] = "New York";
 
-console.log(states);
+// console.log(states);
 
 var user = ["Hassan", "hbrashid@gmail.com", 3, 34, true];
-console.log(user)
+// console.log(user)
 
 user.pop()
+// console.log(user);
+user.unshift("New value");
+// console.log(user);
+user.shift()
 console.log(user);
+
+console.log(user.indexOf(3));
+
+console.log(Array.from("Hassan"));

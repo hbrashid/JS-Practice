a9d0bf434a2929a2038d2618f9421a264975b767
hbrashid <hbrashid@gmail.com>
2020-12-01 23:40:42 -0600

    arrays

---
+++ 03Intermediate/07array.js
@@ -0,0 +1,17 @@
+var countries = ["Pakistan", "USA", "Fiji", "UK"];
+
+var states = new Array("Texas", "Colorado", "California", "Iowa");
+
+console.log(states[0]);
+
+console.log(states.length)
+
+states[3] = "New York";
+
+console.log(states);
+
+var user = ["Hassan", "hbrashid@gmail.com", 3, 34, true];
+console.log(user)
+
+user.pop()
+console.log(user);
